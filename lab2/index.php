78a540f98ccf5d650cefc257ccbd0c6643e70cb2
klara999 <klarita.milly.606@gmail.com>
2026-03-15 02:43:28 +0300

    Update index.php

--- lab2/index.php
+++ lab2/index.php
@@ -5,7 +5,7 @@
 <title>GPA Calculator</title>
 
 <link rel="stylesheet"
-href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+href="style.css">
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="script.js"></script>
